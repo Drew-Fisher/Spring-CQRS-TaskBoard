813411165a5aed4bb9aec7d97d3dcdf576c98fea
Drew <Drewfisher717@gmail.com>
2022-06-08 00:30:48 -0400

    Refactoring prep testController

--- TaskBoard/src/main/java/io/taskboard/TaskBoard/query/controller/testController.java
+++ TaskBoard/src/main/java/io/taskboard/TaskBoard/query/controller/testController.java
@@ -4,25 +4,21 @@ import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
 import io.taskboard.TaskBoard.query.dao.TaskEntity;
-import io.taskboard.TaskBoard.query.repository.TaskReadRepository;
+import io.taskboard.TaskBoard.query.features.PublishQuery.IQueryPublisher;
 
 import java.util.UUID;
 
 @DgsComponent
 public class testController {
-    private final TaskReadRepository taskReadRepository;
 
-    public testController(TaskReadRepository taskReadRepository) {
-        this.taskReadRepository = taskReadRepository;
+    private final IQueryPublisher queryPublisher;
+
+    public testController(IQueryPublisher queryPublisher) {
+        this.queryPublisher = queryPublisher;
     }
 
     @DgsQuery(field = "testGet")
     public TaskEntity test(@InputArgument("id") String id){
-        UUID in = UUID.fromString(id);
-        System.out.println(id);
-        boolean ispres = taskReadRepository.existsById(in);
-        System.out.println(ispres);
-        TaskEntity t = taskReadRepository.findAll().iterator().next();
-        return t;
+        return null;
     }
 }
